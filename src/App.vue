8b015611cef6729fc40a186129f8ee087c3c7d35
shirleyxwyc <shirleyxwyc@gmail.com>
2026-03-03 20:52:00 +0800

    add Element-plus On-demand Import

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
+  <el-button type="primary" disabled>Primary</el-button>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     //作用：Vite 要知道 @ 係 ./src
     alias: {
