a5d2ca7cac1825ffc647a876b07aa080a566f587
shirleyxwyc <shirleyxwyc@gmail.com>
2026-03-05 16:14:40 +0800

    static structure-Nav,Header,Footer

---
+++ src/views/Layout/components/LayoutNav.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="app-topnav">
+    <div class="container">
+      <ul>
+        <template v-if="true">
+          <li>
+            <a><i class="iconfont icon-user"></i>周杰倫</a>
+          </li>
+          <li>
+            <el-popconfirm
+              title="確認退出嗎？"
+              confirm-button-text="確認"
+              cancel-button-text="取消"
+            >
+              <template #reference>
+                <a>退出登錄</a>
+              </template>
+            </el-popconfirm>
+          </li>
+          <li><a>我的訂單</a></li>
+          <li><a>會員中心</a></li>
+        </template>
+        <template v-else>
+          <li><a @click="$router.push('/login')">請先登錄</a></li>
+          <li><a>幫助中心</a></li>
+          <li><a>關於我们</a></li>
+        </template>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,18 @@
+<script setup>
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutNav from './components/LayoutNav.vue'
+</script>
+
 <template>
   <div>
     首頁
+    <layout-nav />
+    <layout-header />
     <!-- 二級路由出口 -->
     <router-view />
+    <layout-footer />
   </div>
 </template>
-
-<script setup></script>
 
 <style scoped></style>
